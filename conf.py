59251bc24eac64b9e602057b4e28795d15d23e2f
Till Hoffmann <tillahoffmann@gmail.com>
2023-05-17 12:13:49 -0400

    Add documentation and doctests.

---
+++ conf.py
@@ -0,0 +1,11 @@
+project = "snippets"
+master_doc = "README"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+]
+exclude_patterns = [
+    ".git",
+    ".pytest_cache",
+]
