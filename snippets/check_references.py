8c7660a852601579cf4c0730c24c1bd162d85f1c
Till Hoffmann <tillahoffmann@gmail.com>
2023-06-06 11:34:50 -0400

    Add flag to check dois.

--- snippets/check_references.py
+++ snippets/check_references.py
@@ -2,42 +2,62 @@ import argparse
 from pathlib import Path
 import re
 from typing import List, Optional
+from .util import raise_for_missing_modules
+
+
+with raise_for_missing_modules():
+    import requests
 
 
 BIB_PATTERN = re.compile(r"@\w+\{(.*?),")
 BBL_PATTERN = re.compile(r"(?:\\bibitem(?:\[.*?\])?\{(.*?)\})|(?:\\entry\{(.*?)\})", re.S)
+DOI_PATTERN = re.compile(r"doi\s*=\s*\{\s*(.*?)\s*\},")
 
 
 class Args:
     bib: Path
     bbl: Path
+    check_dois: bool
 
 
 def __main__(arg_list: Optional[List[str]] = None) -> None:
     parser = argparse.ArgumentParser(description="check for missing or unused references")
     parser.add_argument("bib", help="bibtex file containing references", type=Path)
-    parser.add_argument("bbl", help="compiled bibliography of formatted references", type=Path)
+    parser.add_argument("bbl", help="compiled bibliography of formatted references", nargs="?",
+                        type=Path)
+    parser.add_argument("--check-dois", action="store_true", help="check dois in the bib file")
     args: Args = parser.parse_args(arg_list)
 
     bib = args.bib.read_text()
     bib_refs = set(BIB_PATTERN.findall(bib))
     print(f"found {len(bib_refs)} bib references")
 
-    bbl = args.bbl.read_text()
-    bbl_refs = set(map(max, BBL_PATTERN.findall(bbl)))
-    print(f"found {len(bbl_refs)} bbl references")
+    # Check consistency of references if a bbl file is given.
+    if args.bbl:
+        bbl = args.bbl.read_text()
+        bbl_refs = set(map(max, BBL_PATTERN.findall(bbl)))
+        print(f"found {len(bbl_refs)} bbl references")
 
-    extra = bib_refs - bbl_refs
-    if extra:
-        print(f"{len(extra)} extra references: {', '.join(sorted(extra))}")
-    else:
-        print("no extra references")
+        extra = bib_refs - bbl_refs
+        if extra:
+            print(f"{len(extra)} extra references: {', '.join(sorted(extra))}")
+        else:
+            print("no extra references")
 
-    missing = bbl_refs - bib_refs
-    if missing:
-        print(f"{len(missing)} missing references: {', '.join(sorted(missing))}")
-    else:
-        print("no missing references")
+        missing = bbl_refs - bib_refs
+        if missing:
+            print(f"{len(missing)} missing references: {', '.join(sorted(missing))}")
+        else:
+            print("no missing references")
+
+    # Check dois if desired.
+    if args.check_dois:
+        dois = re.findall(DOI_PATTERN, bib)
+        for doi in dois:
+            headers = {"Accept": "application/json"}
+            response = requests.get(f"https://dx.doi.org/{doi}", headers=headers)
+            if response.status_code != 200:
+                print(f"doi {doi} could not be resolved")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test_check_references.py
+++ tests/test_check_references.py
@@ -20,11 +20,25 @@ from unittest import mock
         r"\bibitem[{Bishop(2006)}]{Bishop2006} \bibitem{key}",
         [r"missing references: Bishop2006", r"extra references: other"]),
 ])
-def test_check_references(bib: str, bbl: str, patterns: List[str], capsys: pytest.CaptureFixture) \
-        -> None:
+def test_check_references_bbl(bib: str, bbl: str, patterns: List[str],
+                              capsys: pytest.CaptureFixture) -> None:
     with mock.patch("pathlib.Path.read_text", side_effect=[bib, bbl]):
         __main__(["bib-file", "bbl-file"])
 
     outerr = capsys.readouterr()
     for pattern in patterns:
         assert re.search(pattern, outerr.out)
+
+
+def test_check_references(capsys: pytest.CaptureFixture) -> None:
+    bib = """
+    doi = {10.48550/arXiv.1804.06788},
+    ...
+    doi = {bla-bla},
+    """
+    with mock.patch("pathlib.Path.read_text", return_value=bib):
+        __main__(["bib-file", "--check-dois"])
+
+    outerr = capsys.readouterr()
+    assert "10.48550/arXiv.1804.06788" not in outerr.out
+    assert "bla-bla" in outerr.out
