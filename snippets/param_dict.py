2e9fc9dd1ef79c8e9776807d90496c5f83cf82c9
Till Hoffmann <tillahoffmann@gmail.com>
2023-06-25 13:27:39 -0400

    Add parameter dictionary conversion to/from tensor.

---
+++ snippets/param_dict.py
@@ -0,0 +1,87 @@
+from functools import reduce
+from operator import mul
+from typing import Dict, Tuple, TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    import numpy as np
+    import torch as th
+
+    TensorLike = np.ndarray | th.Tensor
+    ParamDict = Dict[str, TensorLike]
+
+ShapeDict = Dict[str, Tuple[int]]
+
+
+def from_param_dict(params: "ParamDict", shapes: ShapeDict) -> "TensorLike":
+    """
+    Convert a dictionary of parameters to a tensor of parameters for batch processing.
+
+    Args:
+        params: Dictionary mapping parameter names to tensors.
+        shapes: Dictionary mapping parameter names to shapes.
+
+    Returns:
+        Tensor of concatenated, raveled parameters ordered by parameter name.
+    """
+    if not params:
+        raise ValueError("The parameter dictionary is empty.")
+    if set(params) != set(shapes):
+        raise ValueError("Parameter dictionary keys and shape dictionary keys do not match.")
+
+    batch_shape = None
+    batch_shape_param_name = None
+    parts = []
+    for param, value in sorted(params.items()):
+        # Get the shapes and verify the batch shapes match up.
+        param_shape = shapes[param]
+        param_ndim = len(param_shape)
+        param_batch_shape = value.shape[:len(value.shape) - param_ndim]
+
+        if batch_shape is None:
+            batch_shape = param_batch_shape
+            batch_shape_param_name = param
+        elif batch_shape != param_batch_shape:
+            raise ValueError(f"Expected batch shape {batch_shape} based on "
+                             f"{batch_shape_param_name}; got {param_batch_shape} for {param}.")
+
+        # Collapse the parameter shape.
+        value = value.reshape((*batch_shape, -1))
+        parts.append(value)
+
+    if value.__class__.__name__ == "ndarray":
+        import numpy as np
+        return np.concatenate(parts, axis=-1)
+    else:
+        import torch as th
+        return th.concatenate(parts, axis=-1)
+
+
+def to_param_dict(params: "TensorLike", shapes: ShapeDict) -> "ParamDict":
+    """
+    Convert a tensor of parameters to a dictionary of parameters.
+
+    Args:
+        params: Tensor of concatenated, raveled parameters ordered by parameter name.
+        shapes: Dictionary mapping parameter names to shapes.
+
+    Returns:
+        Dictionary mapping parameter names to tensors with the specified shape.
+    """
+    if not shapes:
+        raise ValueError("The shape dictionary is empty.")
+
+    size = sum(reduce(mul, shape, 1) for shape in shapes.values())
+    if params.shape[-1] != size:
+        raise ValueError(f"Expected {size} elements based on shape dictionary; got "
+                         f"{params.shape[-1]}")
+
+    parts = {}
+    offset = 0
+    batch_shape = params.shape[:-1]
+    for param, shape in sorted(shapes.items()):
+        size = reduce(mul, shape, 1)
+        parts[param] = params[..., offset:offset + size].reshape((*batch_shape, *shape))
+        offset += size
+
+    return parts
