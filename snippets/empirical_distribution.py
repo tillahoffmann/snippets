3d0e2059bd634be709739879046fa751c8862bd6
Till Hoffmann <tillahoffmann@gmail.com>
2023-06-28 16:05:55 -0400

    Add function to normalize cumulative distribution function.

--- snippets/empirical_distribution.py
+++ snippets/empirical_distribution.py
@@ -10,9 +10,39 @@ with raise_for_missing_modules():
     from scipy import integrate, interpolate
 
 
+def normalize_cdf(cdf: "TensorLike", tol: Optional[float] = None) -> np.ndarray:
+    """
+    Normalize a cumulative distribution function between 0 and 1 after validation.
+
+    Args:
+        cdf: Cumulative distribution function to normalize.
+        tol: If given, the maximum acceptable difference for the first value of `cdf` to differ from
+            0 and the last value to differ from 1. Discrepancies may arise, for example, due to
+            numerical errors incurred integrating a probability distribution function to obtain
+            `cdf`.
+
+    Returns:
+        Normalized cumulative distribution function.
+    """
+    if np.diff(cdf).min() < 0:
+        raise ValueError("`cdf` must be non-decreasing.")
+
+    if tol is not None:
+        if abs(cdf[0]) > tol:
+            raise ValueError(f"`cdf` must start with 0 (currently using tolerance {tol}); got "
+                             f"{cdf[0]}.")
+        if abs(cdf[-1] - 1) > tol:
+            raise ValueError(f"`cdf` must end with 1 (currently using tolerance {tol}); got "
+                             f"{cdf[-1]}.")
+
+    cdfmin = cdf.min()
+    return (cdf - cdfmin) / (cdf.max() - cdfmin)
+
+
 def sample_empirical_cdf(x: "TensorLike", cdf: "TensorLike",
                          size: Optional[Union[int, Tuple[int]]] = None, kind: str = "linear",
-                         random_state: Optional[np.random.RandomState] = None) -> np.ndarray:
+                         random_state: Optional[np.random.RandomState] = None, tol: float = 1e-9) \
+        -> np.ndarray:
     """
     Sample from a univariate empirical cumulative distribution function using interpolation of the
     inverse cumulative distribution function.
@@ -23,18 +53,23 @@ def sample_empirical_cdf(x: "TensorLike", cdf: "TensorLike",
         size: Sample size to draw.
         kind: Interpolation method to use (see :class:`scipy.interpolate.inter1pd` for details).
         random_state: Random number generator state.
+        tol: Tolerance for normalizing `cdf` (see :func:`.normalize_cdf` for details).
 
     Returns:
         Sample the desired size.
     """
+    if np.diff(x).min() <= 0:
+        raise ValueError("`x` must be strictly increasing.")
+
     random_state = random_state or np.random
-    interpolated = interpolate.interp1d(cdf, x, kind=kind, assume_sorted=True)
+    interpolated = interpolate.interp1d(normalize_cdf(cdf, tol), x, kind=kind, assume_sorted=True)
     return interpolated(random_state.uniform(size=size))
 
 
 def sample_empirical_pdf(x: "TensorLike", pdf: "TensorLike",
                          size: Optional[Union[int, Tuple[int]]] = None, kind: str = "linear",
-                         random_state: Optional[np.random.RandomState] = None) -> np.ndarray:
+                         random_state: Optional[np.random.RandomState] = None, tol: float = 0) \
+        -> np.ndarray:
     """
     Sample from a univariate empirical probability distribution function using interpolation of the
     inverse cumulative distribution function.
@@ -45,6 +80,9 @@ def sample_empirical_pdf(x: "TensorLike", pdf: "TensorLike",
         size: Sample size to draw.
         kind: Interpolation method to use (see :class:`scipy.interpolate.inter1pd` for details).
         random_state: Random number generator state.
+        tol: Maximum acceptable difference for the integrated cumulative distribution function to
+            differ from 0 and the last value to differ from 1. Discrepancies may arise, for example,
+            due to numerical errors incurred integrating `pdf`.
 
     Returns:
         Sample the desired size.
@@ -53,4 +91,4 @@ def sample_empirical_pdf(x: "TensorLike", pdf: "TensorLike",
     # returns `n - 1` elements for `n`-length input.
     cdf = np.zeros(x.size)
     cdf[1:] = integrate.cumulative_trapezoid(pdf, x)
-    return sample_empirical_cdf(x, cdf, size, kind, random_state)
+    return sample_empirical_cdf(x, cdf, size, kind, random_state, tol)
--- tests/test_empirical_distribution.py
+++ tests/test_empirical_distribution.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 from scipy import stats
-from snippets.empirical_distribution import sample_empirical_pdf
+from snippets.empirical_distribution import normalize_cdf, sample_empirical_pdf
 from typing import Optional, Tuple, Union
 
 
@@ -17,14 +17,14 @@ def pdf(x: np.ndarray):
 
 @pytest.mark.parametrize("kind", ["linear", "quadratic", "cubic"])
 def test_sample_empirical_pdf(x: np.ndarray, pdf: np.ndarray, kind: str) -> None:
-    y = sample_empirical_pdf(x, pdf, (100, 17), kind)
+    y = sample_empirical_pdf(x, pdf, (100, 17), kind, tol=1e-5)
     assert y.shape == (100, 17)
     assert stats.normaltest(y.ravel()).pvalue > 0.01
     assert np.unique(y).size == 1_700
 
 
 def test_sample_empirical_pdf_without_shape(x: np.ndarray, pdf: np.ndarray) -> None:
-    assert sample_empirical_pdf(x, pdf).shape == ()
+    assert sample_empirical_pdf(x, pdf, tol=1e-5).shape == ()
 
 
 @pytest.mark.parametrize("shape", [
@@ -35,6 +35,34 @@ def test_sample_empirical_pdf_without_shape(x: np.ndarray, pdf: np.ndarray) -> N
 ])
 def test_sample_empirical_pdf_shapes(x: np.ndarray, pdf: np.ndarray,
                                      shape: Optional[Union[Tuple[int], int]]) -> None:
-    y = sample_empirical_pdf(x, pdf, shape)
+    y = sample_empirical_pdf(x, pdf, shape, tol=1e-5)
     expected_shape = () if shape is None else ((shape,) if isinstance(shape, int) else shape)
     assert y.shape == expected_shape
+
+
+def test_rv_non_increasing() -> None:
+    with pytest.raises(ValueError, match="strictly increasing"):
+        sample_empirical_pdf(np.zeros(3), np.zeros(3) * np.nan)
+
+
+def test_normalize_cdf() -> None:
+    cdf = np.linspace(0, 1)
+    np.testing.assert_allclose(normalize_cdf(cdf), cdf)
+    # Normalize without validation.
+    np.testing.assert_allclose(normalize_cdf(1 + cdf), cdf)
+
+    # Complain about non-monotone cdf.
+    with pytest.raises(ValueError, match="must be non-decreasing"):
+        normalize_cdf(cdf[::-1], tol=0)
+
+    # Complain about the initial value.
+    with pytest.raises(ValueError, match="must start with 0"):
+        normalize_cdf(1 + cdf, tol=0)
+    with pytest.raises(ValueError, match="must start with 0"):
+        normalize_cdf(cdf - 1, tol=0)
+
+    # Complain about the final value.
+    with pytest.raises(ValueError, match="must end with 1"):
+        normalize_cdf(cdf / 2, tol=0)
+    with pytest.raises(ValueError, match="must end with 1"):
+        normalize_cdf(2 * cdf, tol=0)
